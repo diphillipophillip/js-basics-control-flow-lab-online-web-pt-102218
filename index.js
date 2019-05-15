9f58524e6226c2cb2c3f1aaa509c45fe6ec1fa36
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 00:03:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function scuberGreetingForFeet(someValue){
  if (someValue <= 400)
- return 'This one is on me!' 
+ 'This one is on me!' 
 } else if (someValue > 2000) {
-  return 'I will gladly take your thirty bucks'
+  'I will gladly take your thirty bucks'
 }
 
 function ternaryCheckCity(){
