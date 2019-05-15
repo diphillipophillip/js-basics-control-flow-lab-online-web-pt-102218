644480245a7b3df193e96a66b5bf1c93bb4e6494
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 00:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function scuberGreetingForFeet(someValue) {
   return 'I will gladly take your thirty bucks.';
 } else if (someValue > 2500) {
   return 'No can do.';
-}
+} 
+
   
 
 function ternaryCheckCity(){
