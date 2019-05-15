c527dafa8ba85b113402f0a99cfe9c5dda64cf5e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 00:49:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet(someValue) {
 
 
 function ternaryCheckCity(city){
-  return city = 'NYC' ('Ok, sounds good.') : ('No go'.);
+  return city === 'NYC' ? ('Ok, sounds good.') : ('No go'.);
 }
 
 function switchOnCharmFromTip(){
